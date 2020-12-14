30475a9c936062fd39253924c20c484742bb6f6d
RanZhou <zhour06lzu@gmail.com>
2020-12-13 21:51:32 -0500

    Add files via upload

---
+++ Day13_crack_p2.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+my $file=shift;
+open(IN,$file)||die $!;
+my $size=0;
+my $cmp=1;
+my %residual;
+while(<IN>){
+	chomp;
+	my @line=split(/\t/,$_);
+	##Two columns:BusPositionIntheColumn(Residual) Then BusID(Prime Number)
+	if($line[0] == 0){
+		$residual{$line[1]}=0;
+	}elsif($line[0]<$line[1]){
+		$residual{$line[1]}=$line[1]-$line[0];
+	}else{
+		$residual{$line[1]}=$line[1]-($line[0]%$line[1]);
+	}
+	$cmp=$cmp*$line[1];
+}
+print "$cmp\n";
+my $acc_sum=0;
+foreach my $p (sort {$a<=>$b} keys %residual){
+	my $revp=$cmp/$p;
+	my $modrev=rev_mod($revp,$p);
+	$acc_sum=$acc_sum+($residual{$p}*$modrev*$revp);
+	print "$p,$residual{$p},$revp,$modrev,$acc_sum\n";
+}
+my $numberS=$acc_sum%$cmp;
+print "$acc_sum,$numberS\n";
+
+sub rev_mod{
+	(my $revpn,my $pn)=@_;
+    if($revpn % $pn ==1){
+		return 1;
+	}else{
+		for(my $i=1;$i<$pn;$i++){
+			if($i*$revpn % $pn ==1){
+				return $i;
+				last;
+			}
+		}
+	}
+}
